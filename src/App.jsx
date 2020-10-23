121adea4de7e29122afd38fc7a82f245a4404c4e
lidiyacheb <lidia.saulite@gmail.com>
2020-10-23 12:04:19 +0200

    create addStudent method

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,10 @@ class App extends React.Component {
   }
 
   /*Write an addStudent method here*/
-
+  addAssignment(assignmentName) {
+    this.setState({
+      assignments: this.state.assignments.concat(assignmentName)
+    });
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
     let assignmentName = assignment;
